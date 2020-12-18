1f69b312e6b79d5afe3fcc0511ac4d71eaf43aa0
Conaun <connaunu@outlook.com>
2020-12-18 08:50:36 +0800

    tmp

---
+++ findDiff_20201218.py
@@ -0,0 +1,18 @@
+from typing import List
+# https://leetcode-cn.com/problems/find-the-difference/
+
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        dicto = dict()
+        for each in s:
+
+
+        
+
+
+s = input()
+t = input()
+
+print(Solution().findTheDifference(s, t))
+
+# print(1)
